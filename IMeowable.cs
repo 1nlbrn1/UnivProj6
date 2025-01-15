3123f886b0825bf0195ccf33db0c0f37a3522cdf
1nlbrn1 <86798663+1nlbrn1@users.noreply.github.com>
2025-01-15 11:58:11 +0500

    Create IMeowable.cs

---
+++ IMeowable.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnivLabProj6 {
+    interface IMeowable {
+        public void meow();
+        public void meow(int n);
+    }
+}
